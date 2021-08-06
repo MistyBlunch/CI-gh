25235f8376d9d069aabc2e504199cc0ed93c5190
MistyBlunch <gracenikole@gmail.com>
2021-08-06 09:46:47 -0500

    ang

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  user = {
+    name : "Grace",
+    lastname: "Alvarado",
+    nick: "Misty Blunch"
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
