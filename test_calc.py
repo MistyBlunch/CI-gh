4fce67c3592df7a03aec6fa1be383c9da13849f0
MistyBlunch <gracenikole@gmail.com>
2021-08-10 09:38:33 -0500

    jajsjs

--- test_calc.py
+++ test_calc.py
@@ -4,15 +4,16 @@ Unit tests for the calc library
 
 import calc
 
+
 class TestCalc:
-  def test_addition(self):
-    assert 4 == calc.add(2, 2)
+    def test_addition(self):
+        assert 4 == calc.add(2, 2)
 
-  def test_subtraction(self):
-    assert 2 == calc.subtract(4, 2)
+    def test_subtraction(self):
+        assert 2 == calc.subtract(4, 2)
 
-  def test_multiply(self):
-    assert 6 == calc.multiply(3, 2)
+    def test_multiply(self):
+        assert 6 == calc.multiply(3, 2)
 
-  def test_divide(self):
-    assert 1 == calc.divide(2, 2)
+    def test_divide(self):
+        assert 1 == calc.divide(2, 2)
