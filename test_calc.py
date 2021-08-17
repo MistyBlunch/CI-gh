f3f57043b9803664004ad74e28b0eca3ca969976
MistyBlunch <gracenikole@gmail.com>
2021-08-16 19:40:44 -0500

    test

--- test_calc.py
+++ test_calc.py
@@ -7,7 +7,7 @@ import calc
 
 class TestCalc:
     def test_addition(self):
-        assert 4 == calc.add(2, 2)
+        assert 3 == calc.add(2, 2)
 
     def test_subtraction(self):
         assert 2 == calc.subtract(4, 2)
